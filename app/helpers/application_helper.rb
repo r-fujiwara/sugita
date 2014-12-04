aca890cff3b7ba47c6a2553144579fe038c5b099
r-fujiwara <r-fujiwara@nekojarashi.com>
2014-12-04 16:10:09 +0900

    add user image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def avatar_from(user_id:)
+    user = user_attrs user_id: user_id
+    "https://avatars6.githubusercontent.com/#{user.name}"
+  end
+
+  def user_attrs(user_id:)
+    user = User.find(user_id)
+  end
 end
